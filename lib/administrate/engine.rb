557cd3789f3d788d54ac9c1099f272a297b74a1a
Alexander Grebennik <aleks.grebennik@gmail.com>
2021-04-28 22:50:08 +0300

    remove precompile option, because manifest.js exists

--- lib/administrate/engine.rb
+++ lib/administrate/engine.rb
@@ -22,13 +22,6 @@ module Administrate
     @@javascripts = []
     @@stylesheets = []
 
-    initializer "administrate.assets.precompile" do |app|
-      app.config.assets.precompile += [
-        "administrate/application.js",
-        "administrate/application.css",
-      ]
-    end
-
     def self.add_javascript(script)
       @@javascripts << script
     end
